8c04dfcfcfff82904b96d4372e741b04e869a37f
Adam Caine <adam.caine10@gmail.com>
2022-02-18 20:30:20 +0000

    Move render code from main.c to render_master.c

---
+++ src/render/render_master.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <cglm/cglm.h>
+
+#include "log/log.h"
+#include "log/proginfo.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main() {
+  logging_init(5);
+  preamble();
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Window!", NULL, NULL);
+	if (window == NULL) {
+		printf("Failed to create GLFW window!\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		printf("Failed to initialize GLAD\n");
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Simple code to test cglm functionality
+	mat4 m = GLM_MAT4_IDENTITY_INIT;
+
+	glm_mat4_mul(m, m, m);
+
+	while(!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
