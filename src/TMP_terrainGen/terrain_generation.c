015aec4d0e0e560d536250b460e6f556e4edee5d
Toby Godfrey <toby.godfrey2003@gmail.com>
2022-02-18 17:18:57 +0000

    CAV-25, Now uses clog

--- src/TMP_terrainGen/terrain_generation.c
+++ src/TMP_terrainGen/terrain_generation.c
@@ -1,4 +1,5 @@
 #include "terrain_generation.h"
+#include "../log/log.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,5 +8,6 @@ struct room room_init() {
     .tile_layout = (enum tile_type*) calloc(CAVERNA_LEVEL_X_DIMENSION*CAVERNA_LEVEL_Y_DIMENSION*CAVERNA_LEVEL_Y_DIMENSION, sizeof(enum tile_type)),
     .level = -1,
   };
+  clog(CAVERNA_LOG_LEVEL_DEBUG, "room struct successfully created");
   return room;
 }
