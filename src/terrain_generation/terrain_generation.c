be6e65ee972dbc990aeb16405565e3dd8def2e97
Toby Godfrey <toby.godfrey2003@gmail.com>
2022-02-18 21:25:28 +0000

    CAV-25, changed log level

--- src/terrain_generation/terrain_generation.c
+++ src/terrain_generation/terrain_generation.c
@@ -10,17 +10,10 @@ struct room room_init(uint8_t level) {
     .tile_layout = {{{WALL}}},
     .level = level,
   };
-  clog(CAVERNA_LOG_LEVEL_DEBUG, "room struct successfully created");
+  clog(CAVERNA_LOG_LEVEL_TRACE, "Room struct successfully created");
   return room;
 }
 
 void generate_level(struct room *room) {
-  for(uint8_t x = 0; x < CAVERNA_LEVEL_X_DIMENSION; x++){
-    for(uint8_t y = 0; y < CAVERNA_LEVEL_Y_DIMENSION; y++){
-      for(uint8_t z = 0; z < CAVERNA_LEVEL_Z_DIMENSION; z++){
-        clogf(CAVERNA_LOG_LEVEL_DEBUG, "[%d][%d][%d]", x, y, z);
-        clogf(CAVERNA_LOG_LEVEL_DEBUG, "%d", room->tile_layout[x][y][z]);
-      }
-    }
-  }
+  // TODO: GENERATE A LEVEL
 }
