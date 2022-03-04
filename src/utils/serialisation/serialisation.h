63cce31e91a5f634fd3576a0504b7532943a7609
Toby Godfrey <toby.godfrey2003@gmail.com>
2022-03-04 21:00:50 +0000

    CAV-33, prelim method sigs

--- src/utils/serialisation/serialisation.h
+++ src/utils/serialisation/serialisation.h
@@ -3,7 +3,24 @@
  *
  * @author Toby Godfrey
  */
+
+///
+/// Tell me of any structs for which you want methods implemented
+///
+
 #ifndef SERIALISATION_H
 #define SERIALISATION_H
+
+#include <stdint.h>
+
+uint8_t* serialise_room(struct room room);
+
+uint8_t* serialise_int(int num);
+
+uint8_t* serialise_string(char* msg);
+
+uint8_t serialise_char(char character);
+
+uint8_t serialise_double(double num);
 
 #endif // SERIALISATION_H
