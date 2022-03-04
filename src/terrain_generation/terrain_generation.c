f6feeefd658dd553496684093177eff965eab355
Toby Godfrey <toby.godfrey2003@gmail.com>
2022-03-04 12:06:39 +0000

    Fixed for pull request #9

--- src/terrain_generation/terrain_generation.c
+++ src/terrain_generation/terrain_generation.c
@@ -2,9 +2,9 @@
 
 #include <assert.h>
 #include <stdint.h>
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "../log/log.h"
 
@@ -20,11 +20,9 @@ struct room room_init (uint8_t level, uint8_t x, uint8_t y, uint8_t z) {
 
   if (x < 3 || y < 3 || z < 3) {
     clog(CAVERNA_LOG_LEVEL_ERROR, "Room too small, room initialisation failed");
-  }
-  else if (x > CAVERNA_LEVEL_MAX_X_DIMENSION || y > CAVERNA_LEVEL_MAX_Y_DIMENSION || z > CAVERNA_LEVEL_MAX_Z_DIMENSION) {
+  } else if (x > CAVERNA_LEVEL_MAX_X_DIMENSION || y > CAVERNA_LEVEL_MAX_Y_DIMENSION || z > CAVERNA_LEVEL_MAX_Z_DIMENSION) {
     clog(CAVERNA_LOG_LEVEL_ERROR, "Room larger than maximum size, room initialisation failed");
-  }
-  else {
+  } else {
     room.level = level;
     room.x = x;
     room.y = y;
