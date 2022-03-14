58ca86f345e79f24885ed4fc3a0951d7e1fcd8f8
Toby Godfrey <toby.godfrey2003@gmail.com>
2022-03-14 11:42:21 +0000

    why is this not in c++

--- src/utils/serialisation/serialisation.c
+++ src/utils/serialisation/serialisation.c
@@ -2,8 +2,17 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "../log/log.h"
 
+uint8_t* serialise_int(int num){
+  uint8_t* bitstring = calloc(sizeof(uint8_t), sizeof(num)+64);
+  uint64_t timestamp = time(NULL);
 
-uint8_t* serialise(struct{}*){}
+  for(uint8_t i = 0; i < 64; i++){
+    bitstring[i] = timestamp;
+  }
+
+  return bitstring;
+}
--- src/utils/serialisation/serialisation.h
+++ src/utils/serialisation/serialisation.h
@@ -8,19 +8,51 @@
 /// Tell me of any structs for which you want methods implemented
 ///
 
+/// The first 64 bits of the bitstring are the timestamp
+
 #ifndef SERIALISATION_H
 #define SERIALISATION_H
 
 #include <stdint.h>
 
+/**
+  * @brief Serialise a room struct.
+  *
+  * @param room is the room to be serialised
+  * @return a bitstring
+  */
 uint8_t* serialise_room(struct room room);
 
+/**
+  * @brief Serialise an integer.
+  *
+  * @param num is the int to be serialised
+  * @return a bitstring
+  */
 uint8_t* serialise_int(int num);
 
+/**
+  * @brief Serialise a string.
+  *
+  * @param msg is the string to be serialised
+  * @return a bitstring
+  */
 uint8_t* serialise_string(char* msg);
 
-uint8_t serialise_char(char character);
+/**
+  * @brief Serialise a character.
+  *
+  * @param character is the character to be serialised
+  * @return a bitstring
+  */
+uint8_t* serialise_char(char character);
 
-uint8_t serialise_double(double num);
+/**
+  * @brief Serialise a double.
+  *
+  * @param num is the double to be serialised
+  * @return a bitstring
+  */
+uint8_t* serialise_double(double num);
 
 #endif // SERIALISATION_H
