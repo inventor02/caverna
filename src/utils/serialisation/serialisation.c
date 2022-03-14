ee0796e47da6e56da74016fd9a9f1e42612396c4
Toby Godfrey <toby.godfrey2003@gmail.com>
2022-03-14 16:25:56 +0000

    cpp

--- src/utils/serialisation/serialisation.c
+++ src/utils/serialisation/serialisation.c
@@ -10,9 +10,5 @@ uint8_t* serialise_int(int num){
   uint8_t* bitstring = calloc(sizeof(uint8_t), sizeof(num)+64);
   uint64_t timestamp = time(NULL);
 
-  for(uint8_t i = 0; i < 64; i++){
-    bitstring[i] = timestamp;
-  }
-
   return bitstring;
 }
